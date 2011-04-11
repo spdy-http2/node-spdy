e1a3074a67651be29531b9fa63f83187ac1ce9a3
Fedor Indutny <fedor.indutny@gmail.com>
2011-04-12 01:45:53 +0700

    [test, core] test server should be run on NPN-enabled node.js version, Server() invocation w/o arguments should be successfull

--- test/spdy-server.js
+++ test/spdy-server.js
@@ -1,25 +1,36 @@
 var fs = require('fs'),
     http = require('http'),
-    spdy = require('../lib/spdy');
+    spdy = require('../lib/spdy'),
+    tls = require('tls'),
+    buffer = require('buffer').Buffer,
+    NPNProtocols = new Buffer(7);
+
+if (!tls.hasNPN) throw 'You\'re using not NPN-enabled version of node.js';
+
+NPNProtocols[0] = 6;
+NPNProtocols.write('spdy/2', 1);
 
 var options = {
   key: fs.readFileSync(__dirname + '/../keys/spdy-key.pem'),
   cert: fs.readFileSync(__dirname + '/../keys/spdy-cert.pem'),
-  ca: fs.readFileSync(__dirname + '/../keys/spdy-csr.pem')
+  ca: fs.readFileSync(__dirname + '/../keys/spdy-csr.pem'),
+  NPNProtocols: NPNProtocols
 };
 
-http.createServer(function(req, res) {
-  res.writeHead(200, {
-    'Alternate-Protocol': '8081:npn-spdy/2',
-    'Connection': 'close'
-  });
-  res.end();
-  return;
-  res.end('<!DOCTYPE html><html><head>' +
-          '<body></body></html>');
-}).listen(8080);
+var page = new Buffer([
+  '<!DOCTYPE html>',
+  '<html>',
+  '<head>',
+  '</head>',
+  '<body>',
+  '<h1>SPDY via TLS NPN just works!</h1>',
+  '</body>',
+  '</html>'
+].join(''));
 
 spdy.createServer(options, function(req, res) {
-  console.log(req);
-  res.end('hello world!');
+  res.writeHead(200, 'SPDY WORKS!', {
+    'Content-Type': 'text/html'
+  });
+  res.end(page);
 }).listen(8081);
