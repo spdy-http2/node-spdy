de565baf6ce0cbceb722a23abcc633cb780a132d
Fedor Indutny <fedor.indutny@gmail.com>
2012-01-06 21:30:39 +0600

    [example/twitlog] fix message

--- examples/twitlog/routes/index.js
+++ examples/twitlog/routes/index.js
@@ -8,6 +8,6 @@ exports.index = function(req, res){
     notice: req.isSpdy ?
       'Yay! This page was requested via SPDY protocol'
       :
-      'Oh, no... your browser request this page via HTTPS'
+      'Oh, no... your browser requested this page via HTTPS'
   })
 };
