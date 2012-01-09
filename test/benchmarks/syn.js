2ee21c8ed3f282cfe82a0a407a9244c1640c5365
Fedor Indutny <fedor.indutny@gmail.com>
2012-01-09 16:50:35 +0600

    [benchmark] minimize js

--- test/benchmarks/syn.js
+++ test/benchmarks/syn.js
@@ -1,5 +1,4 @@
-var spdy = require('../../lib/spdy'),
-    tls = require('tls'),
+var tls = require('tls'),
     frames = require('../fixtures/frames'),
     keys = require('../fixtures/keys');
 
@@ -9,21 +8,18 @@ var uri = require('url').parse(process.argv[2]),
     url = uri.path;
 
 frames.createSynStream(host, url, function(syn_stream) {
-  var server = spdy.createServer(keys, function(req, res) {
-    res.end('ok');
-  });
-
-  var start = +new Date;
-  batch(port, host, syn_stream, 200, 1000, function() {
+  var start = +new Date,
+      num = 300;
+  batch(port, host, syn_stream, 200, num, function() {
     var end = +new Date;
-    console.log('requests/sec : %d', 10 / (end - start));
+    console.log('requests/sec : %d', 1000 * num / (end - start));
   });
 });
 
 function request(port, host, data, callback) {
   var socket = tls.connect(port, host, {NPNProtocols: ['spdy/2']}, function() {
     socket.write(data);
-    socket.on('data', function() {
+    socket.once('data', function() {
       socket.destroy();
       callback();
     });
@@ -33,17 +29,19 @@ function request(port, host, data, callback) {
 };
 
 function batch(port, host, data, parallel, num, callback) {
-  var left = num;
+  var left = num,
+      done = 0;
 
   for (var i = 0; i < parallel; i++) {
-    run();
+    run(i);
   }
 
-  function run() {
+  function run(i) {
+    left--;
+    if (left < 0) return;
     request(port, host, data, function() {
-      if (--left === 0) return callback();
-      if (left < 0) return;
-      run();
+      if (++done ===  num) return callback();
+      run(i);
     });
   }
 };
