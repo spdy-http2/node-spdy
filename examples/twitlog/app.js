3629c18f9a56a63321bf5092a00c16936ff97ed9
Fedor Indutny <fedor.indutny@gmail.com>
2012-01-09 14:13:25 +0600

    [example/twitlog] use .staticCache middleware

--- examples/twitlog/app.js
+++ examples/twitlog/app.js
@@ -30,6 +30,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
+  app.use(express.staticCache());
   app.use(express.static(__dirname + '/public'));
 });
 
