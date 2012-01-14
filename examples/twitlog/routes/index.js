9c3921c821a7d64c2bb4a9abe2547671fd54a843
Fedor Indutny <fedor.indutny@gmail.com>
2012-01-14 23:25:59 +0600

    [example/twitlog] push stream image only for spdy

--- examples/twitlog/routes/index.js
+++ examples/twitlog/routes/index.js
@@ -7,16 +7,18 @@ var fs = require('fs');
 var image = fs.readFileSync(__dirname + '/../public/images/nodejs.png');
 
 exports.index = function(req, res){
-  // Push stream image
-  res.push(
-    '/images/nodejs.png',
-    { 'content-type': 'image/png' },
-    function(err, stream) {
-      if (err) return;
-      stream.on('error', function() {});
-      stream.end(image);
-    }
-  );
+  if (res.isSpdy) {
+    // Push stream image
+    res.push(
+      '/images/nodejs.png',
+      { 'content-type': 'image/png' },
+      function(err, stream) {
+        if (err) return;
+        stream.on('error', function() {});
+        stream.end(image);
+      }
+    );
+  }
   res.render('index', {
     title: 'SPDY - Twitlog',
     notice: req.isSpdy ?
