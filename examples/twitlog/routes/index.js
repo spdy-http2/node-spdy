c722a8c3ae021d1991b5f8d3ca76cba919d3fd20
Fedor Indutny <fedor.indutny@gmail.com>
2012-01-15 17:19:47 +0600

    [example/twitlog] disable push streams for Firefox

--- examples/twitlog/routes/index.js
+++ examples/twitlog/routes/index.js
@@ -8,16 +8,23 @@ var image = fs.readFileSync(__dirname + '/../public/images/nodejs.png');
 
 exports.index = function(req, res){
   if (res.isSpdy) {
-    // Push stream image
-    res.push(
-      '/images/nodejs.png',
-      { 'content-type': 'image/png' },
-      function(err, stream) {
-        if (err) return;
-        stream.on('error', function() {});
-        stream.end(image);
-      }
-    );
+    var ua = req.headers['user-agent'];
+
+    // Firefox doesn't support push streams now
+    // should be fixed once: https://bugzilla.mozilla.org/show_bug.cgi?id=718210
+    // will reach all it's builds
+    if (!ua || ua.match(/Firefox/i) === null) {
+      // Push stream image
+      res.push(
+        '/images/nodejs.png',
+        { 'content-type': 'image/png' },
+        function(err, stream) {
+          if (err) return;
+          stream.on('error', function() {});
+          stream.end(image);
+        }
+      );
+    }
   }
   res.render('index', {
     title: 'SPDY - Twitlog',
