99b965cd3c0f76f51f01fd102e09693180a2c804
Fedor Indutny <fedor.indutny@gmail.com>
2012-07-19 13:14:20 +0400

    test: fix tests

--- test/unit/parser-test.js
+++ test/unit/parser-test.js
@@ -9,7 +9,13 @@ suite('A Parser of SPDY module', function() {
     var deflate = spdy.utils.createDeflate(),
         inflate = spdy.utils.createInflate();
 
-    parser = new spdy.parser.create(deflate, inflate);
+    parser = new spdy.parser.create({
+      socket: {
+        setNoDelay: function() {}
+      }
+    }, deflate, inflate);
+
+    parser.createFramer(2);
   });
 
   test('should wait for headers initially', function() {
