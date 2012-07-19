76dfea345926b8c79bc810968b9ad8201d81fea3
Fedor Indutny <fedor.indutny@gmail.com>
2012-07-19 13:14:20 +0400

    test: fix tests

--- test/unit/framer-test.js
+++ test/unit/framer-test.js
@@ -9,8 +9,12 @@ suite('A Framer of SPDY module', function() {
       framer;
 
   setup(function() {
-    inflate = spdy.utils.zwrap(spdy.utils.createInflate());
-    deflate = spdy.utils.zwrap(spdy.utils.createDeflate());
+    inflate = spdy.utils.zwrap(spdy.utils.createInflate(
+      spdy.protocol[2].dictionary
+    ));
+    deflate = spdy.utils.zwrap(spdy.utils.createDeflate(
+      spdy.protocol[2].dictionary
+    ));
     framer = new spdy.protocol[2].Framer(deflate, inflate);
   });
 
