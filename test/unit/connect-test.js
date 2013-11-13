b413a1c0b09842ed216195db9bd28257ea6f8d7b
Fedor Indutny <fedor.indutny@gmail.com>
2013-11-13 23:37:37 +0400

    stream: support 'Content-Encoding'

--- test/unit/connect-test.js
+++ test/unit/connect-test.js
@@ -1,5 +1,6 @@
 var assert = require('assert'),
     spdy = require('../../'),
+    zlib = require('zlib'),
     keys = require('../fixtures/keys'),
     https = require('https'),
     tls = require('tls'),
@@ -8,9 +9,25 @@ var assert = require('assert'),
 
 suite('A SPDY Server / Connect', function() {
   var server;
+  var fox = 'The quick brown fox jumps over the lazy dog';
+
   setup(function(done) {
     server = spdy.createServer(keys, function(req, res) {
-      res.end('ok');
+      var comp = req.url === '/gzip' ? zlib.createGzip() :
+                 req.url === '/deflate' ? zlib.createDeflate() :
+                 null;
+
+      if (!comp)
+        return res.end(fox);
+
+      res.writeHead(200, { 'Content-Encoding' : req.url.slice(1) });
+      comp.on('data', function(chunk) {
+        res.write(chunk);
+      });
+      comp.once('end', function() {
+        res.end();
+      });
+      comp.end(fox);
     });
 
     server.listen(PORT, done);
@@ -29,34 +46,48 @@ suite('A SPDY Server / Connect', function() {
       agent: false,
       rejectUnauthorized: false
     }, function(res) {
-      res.on('data', function() {
-        // Ignore incoming data
+      var received = '';
+      res.on('data', function(chunk) {
+        received += chunk;
+      });
+      res.once('end', function() {
+        assert.equal(received, fox);
+        done();
       });
       assert.equal(res.statusCode, 200);
-      done();
     });
     req.end();
   });
 
-  test('should respond on spdy requests', function(done) {
-    var agent = spdy.createAgent({
-      host: '127.0.0.1',
-      port: PORT,
-      rejectUnauthorized: false
-    });
-
-    var req = https.request({
-      path: '/',
-      method: 'GET',
-      agent: agent,
-    }, function(res) {
-      res.on('data', function() {
-        // Ignore incoming data
+  function spdyReqTest(url) {
+    test('should respond on spdy requests on ' + url, function(done) {
+      var agent = spdy.createAgent({
+        host: '127.0.0.1',
+        port: PORT,
+        rejectUnauthorized: false
       });
-      assert.equal(res.statusCode, 200);
-      agent.close();
-      done();
+
+      var req = https.request({
+        path: url,
+        method: 'GET',
+        agent: agent,
+      }, function(res) {
+        var received = '';
+        res.on('data', function(chunk) {
+          received += chunk;
+        });
+        res.once('end', function() {
+          assert.equal(received, fox);
+          agent.close();
+          done();
+        });
+        assert.equal(res.statusCode, 200);
+      });
+      req.end();
     });
-    req.end();
-  });
+  }
+
+  spdyReqTest('/');
+  spdyReqTest('/gzip');
+  spdyReqTest('/deflate');
 });
