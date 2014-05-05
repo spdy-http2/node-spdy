719116e59da4ce181dd318a9d0d46f51286fc4ff
Peter Loer <ploer@acm.org>
2014-05-05 15:43:42 -0700

    Updating server-side CONNECT test to improve cleanup.  Note that this triggers an error on the client side, but ignores it.

--- test/unit/proxy-test.js
+++ test/unit/proxy-test.js
@@ -7,14 +7,15 @@ var assert = require('assert'),
     PORT = 8081;
 
 suite('A SPDY server / Proxy', function() {
-  test( 'should emit connect event on CONNECT requests', function(done) {
+  test('should emit connect event on CONNECT requests', function(done) {
     var agent;
-    var proxyServer = spdy.createServer(keys, function (req, res) {});
+    var proxyServer = spdy.createServer(keys);
     proxyServer.on('connect', function(req, socket) {
       assert.equal(req.method, 'CONNECT');
       assert(socket.isSpdy);
-      proxyServer.close();
-      done();
+      agent.close(function(){
+        proxyServer.close(done);
+      });
     });
 
     proxyServer.listen(PORT, '127.0.0.1', function() {
@@ -32,6 +33,15 @@ suite('A SPDY server / Proxy', function() {
 
       var req = http.request(options);
       req.end();
+
+      req.on('error', function(err) {
+        // Tolerate this error here since I believe this is caused by the
+        //   incomplete handling of CONNECT requests on the client side,
+        //   and this is intended as a test of the server side.
+        if (err.code && err.code === 'ECONNRESET')
+          return;
+        throw err;
+      });
     });
   });
 });
